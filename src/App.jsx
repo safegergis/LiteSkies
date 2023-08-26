825b00afc462e43b5f2c8ad32fde88907a5a0085
Safe Gergis <safegerg@gmail.com>
2023-08-26 16:08:23 -0700

    added device location

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,41 @@
-import { Title, MantineProvider, Flex } from '@mantine/core'
-import { useState } from 'react'
+import { Title, MantineProvider, Center } from '@mantine/core'
+import { useState, useEffect } from 'react'
+import DisplayWeather from './components/DisplayWeather'
 
 
 function App() {
-  const [location, setLocation] = useState("")
+  const [location, setLocation] = useState({
+    lat:"",
+    lon:"",
+  });
+  const getLocationCall = () => {
+    navigator.geolocation.getCurrentPosition((pos) => {
+      console.log(pos.coords.latitude + " " + pos.coords.longitude)
+      setLocation({
+        lat:pos.coordinates.latitude,
+        lon:pos.coordinates.longitude,
+      });
+    },
+    locationError)
+    }
 
+  const locationError = (error) => {
+    alert("Location is unable to be found")
+  }
+  useEffect(() => {
+    getLocationCall();
+    console.log(location)
+  });
   return (
     <MantineProvider>
-      <Flex
-      justify={"center"}
-      >
+      <div>
+        <Center>
           <Title order={1} align='center'> LiteSkies </Title>
-      </Flex>
+          <DisplayWeather location={location}/>
+        </Center>
+      </div>
     </ MantineProvider>
-  )
+  ) 
 }
 
 export default App
--- src/components/DisplayWeather.js
+++ src/components/DisplayWeather.js
@@ -1,5 +1,24 @@
-import { useState } from 'react'
+import { useState, useEffect} from 'react'
 
-export default function DisplayWeather([location]) {
-    
+export default function DisplayWeather({location}) {
+    const [weatherData, setWeatherData] = useState(null)
+
+    const API_KEY = "5f46ba5e597f328209a63e7d39453ff9"
+    const fetchWeather = async () => {
+        const d = new Date()
+        const date = d.getFullYear() + "-" + d.getMonth() + "-" + d.getDate()
+        try {
+            const fetchWeather = await fetch(
+                `https://api.openweathermap.org/data/3.0/onecall/day_summary?lat=${location.lat}&lon=${location.lon}&date=${date}&appid=${API_KEY}`
+            )
+            const data = fetchWeather.json()
+        }
+        catch (error) {
+            console.error('Error fetching data:', error);
+        }
+        useEffect(() => {
+            setWeatherData(data)
+            print(weatherData)
+        });
+    }
 }
