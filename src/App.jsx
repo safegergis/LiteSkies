bf7f056bc7bba89df0e103b38370d3c515bfc07b
Safe Gergis <safegerg@gmail.com>
2023-08-26 22:17:04 -0700

    working on fetching weather

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,22 @@
-import { Title, MantineProvider, Center } from '@mantine/core'
-import { useState, useEffect } from 'react'
-import DisplayWeather from './components/DisplayWeather'
-
+import { Title, MantineProvider, Center } from "@mantine/core";
+import { useState, useEffect } from "react";
+import DisplayWeather from "./components/DisplayWeather";
 
 function App() {
-  const [location, setLocation] = useState({
-    lat:"",
-    lon:"",
-  });
-  const getLocationCall = () => {
-    navigator.geolocation.getCurrentPosition((pos) => {
-      console.log(pos.coords.latitude + " " + pos.coords.longitude)
-      setLocation({
-        lat:pos.coordinates.latitude,
-        lon:pos.coordinates.longitude,
-      });
-    },
-    locationError)
-    }
 
-  const locationError = (error) => {
-    alert("Location is unable to be found")
-  }
-  useEffect(() => {
-    getLocationCall();
-    console.log(location)
-  });
+
   return (
     <MantineProvider>
       <div>
         <Center>
-          <Title order={1} align='center'> LiteSkies </Title>
-          <DisplayWeather location={location}/>
+          <Title order={1} align="center">
+            LiteSkies
+          </Title>
+          <DisplayWeather location={location} />
         </Center>
       </div>
-    </ MantineProvider>
-  ) 
+    </MantineProvider>
+  );
 }
 
-export default App
+export default App;
--- src/components/DisplayWeather.js
+++ src/components/DisplayWeather.js
@@ -1,24 +1,40 @@
-import { useState, useEffect} from 'react'
+import { useState, useEffect } from "react";
 
-export default function DisplayWeather({location}) {
-    const [weatherData, setWeatherData] = useState(null)
+export default function DisplayWeather({ location }) {
+  const [weatherData, setWeatherData] = useState(null);
+  const [location, setLocation] = useState({
+    lat: "",
+    lon: "",
+  });
 
-    const API_KEY = "5f46ba5e597f328209a63e7d39453ff9"
-    const fetchWeather = async () => {
-        const d = new Date()
-        const date = d.getFullYear() + "-" + d.getMonth() + "-" + d.getDate()
-        try {
-            const fetchWeather = await fetch(
-                `https://api.openweathermap.org/data/3.0/onecall/day_summary?lat=${location.lat}&lon=${location.lon}&date=${date}&appid=${API_KEY}`
-            )
-            const data = fetchWeather.json()
+  const API_KEY = "5f46ba5e597f328209a63e7d39453ff9";
+  const fetchWeather = async () => {
+    try {
+      navigator.geolocation.getCurrentPosition(
+        (pos) => {
+          const locationData = {
+            lat: pos.coords.latitude,
+            lon: pos.coords.longitude,
+          };
+          setLocation(locationData);
+          //console.log(location)
+        },
+        (error) => {
+          alert("Location is unable to be found");
         }
-        catch (error) {
-            console.error('Error fetching data:', error);
-        }
-        useEffect(() => {
-            setWeatherData(data)
-            print(weatherData)
-        });
+      );
+
+      const fetchWeather = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${location.lat}&lon=${location.lon}&appid=${API_KEY}`
+      );
+      const data = fetchWeather.json();
+      setWeatherData(data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
     }
+  };
+  useEffect(() => {
+    fetchWeather();
+    console.log(weatherData);
+  }, []);
 }
